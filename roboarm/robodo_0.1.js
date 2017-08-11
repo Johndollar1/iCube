6ff2ef7baca65ff0728a65de8d8b272d9dfbc872
pitvb <pitvb56@outlook.com>
2017-08-11 20:13:06 +0200

    Update robodo_0.1.js
    Added Moves Array

--- roboarm/robodo_0.1.js
+++ roboarm/robodo_0.1.js
@@ -1,3 +1,85 @@
+const iInit = {
+	0: 350,
+	2: 350,
+	4: 350,
+	6: 350,
+	8: 350,
+	10: 350
+};
+const iPosition = {
+	0: 350,
+	2: 350,
+	4: 350,
+	6: 350,
+	8: 350,
+	10: 350
+};
+
+const arrMoves = [iInit]; // erstelle array mit erstem eintrag initial stellung
+
+// danach definieren wir weitere stellungen welche wir nach einander einnehmen wollen
+arrMoves.push({
+	0: 425,
+	2: 425,
+	4: 425,
+	6: 425,
+	8: 425,
+	10: 425
+});
+
+arrMoves.push({
+	0: 289,
+	2: 289,
+	4: 289,
+	6: 289,
+	8: 289,
+	10: 289
+});
+
+// nur noch die hand um die achse drehen:
+arrMoves.push({
+	8: 125
+});
+
+// pause in mü sekunden zwischen den stellungen:
+const pause = 3 * 1000 * 1000;
+
+let promiseChain = pwmDriver.init()
+		.then(function() { return pwmDriver.setPWMFreq(60) });
+
+for (var i = 0; i < arrMoves.length; i++) {
+	// der befehl hier unten ist ganz wichtig damit (zeitlich) später wenn in der promise chain
+	// auf arrMoves[i] zugegriffen wird, i nicht grösser ist als die länge von arrMoves
+	// hier helfen scope variabeln (als let deklariert) um sicher zu sein, dass
+	// zu einem späteren zeitpunkt immernoch auf die gleiche speicherstelle verwiesen wird
+	let moves = arrMoves[i];
+
+	// hier ketten wir alle bewegungsabläufe hinter einander und schieben den promiseChain pointer ans ende der kette:
+	promiseChain = promiseChain.then(function() {
+		// hier drin stünde arrMoves[i], da aber die for loop schon lange ausgeführt wurde bis
+		// wir in diesen teil der promise chain springen, wäre i grösser als die länge von arrMoves
+		// daher benutzen wir hier moves, welches für jede schleifen-iteration eine eindeutige verweisung
+		// aufrecht erhält... JavaScript magic!
+		let servoMoves = [];
+		// durch diese schleife kann man beliebig viele motoren gleichzeitig bewegen (so viele wie im move objekt deklariert sind):
+		for(let key in moves) {
+			// iPosition haben wir ja glaube ich in moveSmooth verwendet um die aktuelle position zu speichern:
+			servoMoves.append(moveSmooth(key, iPosition[key], moves[key]))
+		}
+		// servo moves ist nun ein array aus einzelnen promises die von moveSmooth zurück gegeben wurden, für jeden servo eine promise
+		return Promise.all(servoMoves); // alle servos gleichzeitig bewegen und erst weiter gehen wenn alle fertig sind
+	})
+	.then(function() { return usleep(pause) });
+}
+
+promiseChain.then(function() {
+	console.log('Done with all movements!')
+	rpio.setRoboArmRelaisOff() // ich weiss den befehl nicht mehr genau :)
+})
+.catch(function(err) {
+	console.error(err);
+	rpio.setRoboArmRelaisOff() // ich weiss den befehl nicht mehr genau :)
+})
 
 const makePwmDriver = require('./pwmDriver');
 const pwmDriver = makePwmDriver({address: 0x40, device: '/dev/i2c-1', debug: true});
