a6369ddfad1aa003c80cff7c09765daff8d3543a
Marco <no@email.1234xyz>
2017-08-14 14:31:18 +0200

    start stop via url

--- Website/js/robo.js
+++ Website/js/robo.js
@@ -3,17 +3,18 @@
 	  
 	  $( "#btn1" ).click(function() {
 
-		  $.ajax({
+		 		  
+		  window.location.href = 'video.html';
+});
+	  
+	  		  $.ajax({
     url: 'http://127.0.0.1:1880/test',
+			  data: "stop",
     complete : function(){
-        alert(this.url)
     },
     success: function(result){
     }
 });	  
-		  
-		  window.location.href = 'video.html';
-});
 	  
 	  
 
